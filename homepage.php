6c21555d1fd3175d2c6d91274b93b9342b0aaefb
adriendelaurens <46447698+adriendelaurens@users.noreply.github.com>
2019-10-11 22:37:11 +0200

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<title> ADOPTEZ UN CHATON </title>
+
+<?php include ('header.php');?>
+
+<head>
+<p><b>Bienvenue sur notre site, trouvez le compagnon de vos reves</b></p>
+<p style='text-align: center;'>
+</p>
+</head>
+
+<body>
+
+<img
+    src="Chaton.jpg" 
+    alt=""
+    height="600px" 
+    width="1000px" 
+   
+/>
+	
+
+
+</body>
+
+
+<?php include ('footer.php');?>
+
+</html>

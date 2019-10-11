971e697951ca1aae2f5f6a26f63a15afc9c747e1
Guillaume74 <56159782+Guillaume74@users.noreply.github.com>
2019-10-11 13:18:15 +0100

    Add files via upload

---
+++ Headertrue.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <title>Adopte Un Chaton</title>
+</head>
+<body>
+
+<h1>Adopte Un Chaton</h1>
+
+
+
+<p><b>Bienvenue sur notre site, trouvez le compagnon de vos reves</b></p>
+<p style='text-align: center;'>
+</p>
+
+
+<header>
+	
+<a href = 'page accueil.html'> page d'accueil</a>&nbsp; 
+
+
+
+<a href = 'recherche.html'> recherche	</a>&nbsp;
+
+
+<a href= 'Gamme.html' > Nos Races de Chaton </a>&nbsp;
+
+
+
+
+<header>
+	
+
+<img
+    src="Chaton.jpg" 
+    alt=""
+    height="600px" 
+    width="1000px" 
+/>

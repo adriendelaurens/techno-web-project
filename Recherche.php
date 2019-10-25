8df17d5a585420bb1a252027b58cb18cbcee8fc2
adriendelaurens <46447698+adriendelaurens@users.noreply.github.com>
2019-10-25 14:44:52 +0200

    Add files via upload

---
+++ Recherche.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<hmtl>
+<head>
+	<title> Recherche</title>
+	<link rel="stylesheet" href="Recherche.css" />
+</head>
+<?php include ('header.php')?>
+<body>
+	<div class = "search-bar">
+		<input class ="search-txt" type="text" placeholder="Rechercher un produit">
+		<a class="search-button" href="#"><img class="rechercher-logo" src="Rechercher.png"> </a>
+	</div>
+</body>
+<?php
+include ('footer.php')
+?>
+</html>

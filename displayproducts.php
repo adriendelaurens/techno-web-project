48a8546c68dc071dbb2e681247eeb2ba0dac9ba4
adriendelaurens <46447698+adriendelaurens@users.noreply.github.com>
2019-10-25 20:12:07 +0200

    Update displayproducts.php

--- displayproducts.php
+++ displayproducts.php
@@ -1,1 +1,34 @@
 <!DOCTYPE html>
+
+<html>
+	
+<?php
+include('header.php');
+include('databaseconnection.php');
+?>
+
+
+
+<body>
+<?php 
+
+$query = "SELECT name from products WHERE name LIKE '%".$_POST['produit']."%'";
+
+echo $query;
+
+$response = $bdd->query($query);
+
+while ($results = $response->fetch() ){
+	var_dump($results);
+}
+
+$response->closeCursor() ;
+
+?>
+
+</body>
+
+<?php
+	    include('footer.php');
+?>
+

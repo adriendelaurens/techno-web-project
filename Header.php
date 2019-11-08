f6233adf8ec47cfe1969b15205c2438d9a4ac8c6
CharlesP-0 <56159744+CharlesP-0@users.noreply.github.com>
2019-11-08 20:57:02 +0100

    Add files via upload

--- Header.php
+++ Header.php
@@ -14,32 +14,34 @@
 
 	
 <header>
-
+<table><tr><td>
 <form method="get" action ="index.php" name="homepage">
 <input type="hidden" name="page" value="homepage">
 <input type="submit" value="Page d'accueil" >
-</form>
+</form></td>
 
-
+<td>
 <form method="get" action ="index.php" name="Recherche">
 <input type="hidden" name="page" value="Recherche">
 <input type="submit" value="Recherche" >
-</form>
-
+</form></td>
+<td>
 <form method="get" action ="index.php" name="NosRacesdeChaton">
 <input type="hidden" name="page" value="NosRacesdeChaton">
 <input type="submit" value="NosRacesdeChaton" >
-</form>
-
+</form></td>
+<td>
 <form method="post" action ="index.php" name="cartpage">
 <input type="hidden" name="page" value="cartpage">
 <input type="submit" value="Mon panier" >
-</form>
-
+</form></td>
+<td>
 <form method="post" action ="index.php" name="createaccount">
 <input type ="hidden" name="page" value="createaccount">
 <input type="submit" value="Créer mon compte" >
-</form>
+</form></td>
+</tr>
+</table>
 
 </header>
 

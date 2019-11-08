9654cbbd90227ed0431611834d688a5c5ffd542e
mehdielaiba <56159974+mehdielaiba@users.noreply.github.com>
2019-11-08 16:49:31 +0100

    Update Header.php

--- Header.php
+++ Header.php
@@ -15,20 +15,20 @@
 	
 <header>
 
-<form method="post" action ="index.php">
+<form method="post" action ="index.php" name="homepage">
 <input type="submit" value="Page d'accueil" >
 
-<form method="post" action ="index.php">
+<form method="post" action ="index.php" name="Recherche">
 <input type="submit" value="Recherche" >
 
 
-<form method="post" action ="index.php">
+<form method="post" action ="index.php" name="NosRacesdeChaton">
 <input type="submit" value="Nos Races de Chaton" >
 
-<form method="post" action ="index.php">
+<form method="post" action ="index.php" name="cartpage">
 <input type="submit" value="Mon panier" >
 
-<form method="post" action ="index.php">
+<form method="post" action ="index.php" name="createaccount">
 <input type="submit" value="Créer mon compte" >
 
 </header>

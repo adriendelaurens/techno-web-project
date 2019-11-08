c7d09cf72d8a993e158279692b4832943994fbec
mehdielaiba <56159974+mehdielaiba@users.noreply.github.com>
2019-11-08 16:33:17 +0100

    Update and rename chatpersan.php to NosRacesdeChaton.php

---
+++ NosRacesdeChaton.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+
+
+<head>
+
+<link rel="stylesheet" href="style.css" />
+
+</head>
+
+
+
+    <body>
+	
+	
+	<div id="haut">
+	
+	<h1> Bienvenue sur Adopte un Chaton, trouvez le compagnon de vos reves! </h1>
+	
+	<p>  Il est l'un des principaux animaux de compagnie et compte aujourd'hui une cinquantaine de races differentes. 
+	C'est un petit mammifere carnivore a poil doux, sauvage ou domestique, au museau tres court.
+	Un celebre dicton dit qu'il retombe toujours sur ses propres pattes. 
+	Vous l'avez...?? sur le bout de la langue? 
+	Et oui! C'est biensur <strong> le Chat</strong> !</p>
+	
+	</div>
+
+
+
+	<div id="fond">
+
+	<h2> Voici le chat Persan: </h2>
+	
+	<img src="persan.jpg"  height="250"width"100"  />	
+	
+
+
+
+	<style>
+	table
+	{border-collapse: collapse;}
+	td, th
+	{border: 1px solid black;}
+	</style>
+
+	<table>
+	 <caption> Caracteristiques du chat Persan          </caption>
+	  <thead><tr>
+		<th>Caracteristiques<br  /> </th>
+		<th>Notes<br  /> </th>
+	  <tr><thead>
+
+<!--	  <tfoot><tr>
+		<td>Footer<br  />col1</td>	   
+		<td>Footer<br  />col2</td>
+	  <tr><tfoot> 
+-->
+
+	<tbody><tr>
+		<td>Domestique<br  /></td>
+		<td>4/5<br  /></td>
+
+	</tr><tr>
+		<td>Agressivite<br  /></td>
+		<td>0/5<br  /></td>
+	</tr><tr>
+		<td>Joueur<br  /></td>
+		<td>1.5/5<br  /></td>
+	</tr><tr>
+		<td>Sociable<br  /></td>
+		<td>2/5<br  /></td>
+	</tr><tr>
+		<td>Affection<br  />/Tendresse</td>
+		<td>4/5<br  /></td>
+	
+	</tr></tbody>
+	</table>
+
+
+
+
+	<h3>
+
+	</h3>
+
+	<form method="post" action ="cartpage.php">
+	<input type="submit" value="Ajouter un article au panier" >
+	<img src="bouton-panier.png"  width="30"  height="30">
+	
+
+	</div>
+	
+
+
+
+<div id="fond">
+
+	
+	
+	
+	<h2> Voici le chat Maine coon: </h2>
+	
+	<img src="mainecoon.png"  height="350"width"300"  />	
+	
+
+
+
+	<style>
+	table
+	{border-collapse: collapse;}
+	td, th
+	{border: 1px solid black;}
+	</style>
+
+	<table>
+	 <caption> Caracteristiques du Maine coon</caption>
+	  <thead><tr>
+		<th>Caracteristiques<br  /> </th>
+		<th>Notes<br  /> </th>
+	  <tr><thead>
+
+
+	<tbody><tr>
+		<td>Domestique<br  /></td>
+		<td>3/5<br  /></td>
+
+	</tr><tr>
+		<td>Agressivite<br  /></td>
+		<td>3/5<br  /></td>
+	</tr><tr>
+		<td>Joueur<br  /></td>
+		<td>3/5<br  /></td>
+	</tr><tr>
+		<td>Sociable<br  /></td>
+		<td>2/5<br  /></td>
+	</tr><tr>
+		<td>Affection<br  />/Tendresse</td>
+		<td>2/5<br  /></td>
+	
+	</tr></tbody>
+	</table>
+
+
+
+
+	<h3>
+
+	</h3>
+
+
+
+		<form method="post" action ="cartpage.php">
+	 <input type="submit" value="Ajouter un article au panier" >
+
+
+	</div>
+
+
+
+
+
+
+
+<div id="fond">
+
+	
+	
+	
+	<h2> Voici le Sphynx: </h2>
+	
+	<img src="sphynx.png"  height="350"width"300"  />	
+	
+
+
+
+	<style>
+	table
+	{border-collapse: collapse;}
+	td, th
+	{border: 1px solid black;}
+	</style>
+
+	<table>
+	 <caption> Caracteristiques du Maine coon</caption>
+	  <thead><tr>
+		<th>Caracteristiques<br  /> </th>
+		<th>Notes<br  /> </th>
+	  <tr><thead>
+
+
+	<tbody><tr>
+		<td>Domestique<br  /></td>
+		<td>4/5<br  /></td>
+
+	</tr><tr>
+		<td>Agressivite<br  /></td>
+		<td>3/5<br  /></td>
+	</tr><tr>
+		<td>Joueur<br  /></td>
+		<td>4/5<br  /></td>
+	</tr><tr>
+		<td>Sociable<br  /></td>
+		<td>2/5<br  /></td>
+	</tr><tr>
+		<td>Affection<br  />/Tendresse</td>
+		<td>2/5<br  /></td>
+	
+	</tr></tbody>
+	</table>
+
+
+
+
+	<h3>
+
+	</h3>
+
+
+
+		<form method="post" action ="cartpage.php">
+	 <input type="submit" value="Ajouter un article au panier" >
+
+
+	</div>
+
+
+
+      <?php  ?>
+    </body>
+
+</html>
+
+

46c5957f1d540d5dbd30e1534173ac8cd474bf98
adriendelaurens <46447698+adriendelaurens@users.noreply.github.com>
2019-11-08 20:01:40 +0100

    Update Recherche.php

--- Recherche.php
+++ Recherche.php
@@ -1,18 +1,20 @@
-  
 <!DOCTYPE html>
 <hmtl>
 <head>
 	<title> Recherche</title>
 	<link rel="stylesheet" href="Recherche.css" />
 </head>
-
+<?php include ('header.php');
+include ('databaseconnection.php');
+?>
 <body>
-	<div class = "search-bar">
-		<form method="post" action="displayproducts.php">
-		<input type ="text" placeholder="Rechercher un produit" name="produit"/>
-		<button type="submit" ><img class="rechercher-logo" src="Rechercher.png"> </a>
-		</form>	
-	</div>
+	<form method="get" action ="index.php" name="displayproducts">
+	<input type="hidden" name="page" value="displayproducts">
+	<input type ="text" placeholder="Rechercher un produit" name="produit"/>
+	<button type="submit" ><img class="rechercher-logo" src="Rechercher.png"> </a>
+	
 </body>
-
+<?php
+include ('footer.php')
+?>
 </html>

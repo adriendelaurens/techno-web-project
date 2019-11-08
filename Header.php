a800983deadd7c92b8c463629db1c580fd945d30
adriendelaurens <46447698+adriendelaurens@users.noreply.github.com>
2019-11-08 19:58:44 +0100

    Update Header.php

--- Header.php
+++ Header.php
@@ -14,22 +14,34 @@
 
 	
 <header>
+<?php //<a href="index.php?page=homepage">Page d'accueil</a>
+?>
 
-<form method="post" action ="index.php" name="homepage">
+<form method="get" action ="index.php" name="homepage">
+<input type="hidden" name="page" value="homepage">
 <input type="submit" value="Page d'accueil" >
+</form>
 
-<form method="post" action ="index.php" name="Recherche">
+
+<form method="get" action ="index.php" name="Recherche">
+<input type="hidden" name="page" value="Recherche">
 <input type="submit" value="Recherche" >
+</form>
 
-
-<form method="post" action ="index.php" name="NosRacesdeChaton">
-<input type="submit" value="Nos Races de Chaton" >
+<form method="get" action ="index.php" name="NosRacesdeChaton">
+<input type="hidden" name="page" value="NosRacedeChaton">
+<input type="submit" value="NosRacesdeChaton" >
+</form>
 
 <form method="post" action ="index.php" name="cartpage">
+<input type="hidden" name="page" value="cartpage">
 <input type="submit" value="Mon panier" >
+</form>
 
 <form method="post" action ="index.php" name="createaccount">
+<input type ="hidden" name="page" value="createaccount">
 <input type="submit" value="Créer mon compte" >
+</form>
 
 </header>
 
